8a691ed8b87a6fc25ec99e3b911be10f342081e1
Nancy Jimenez-Garcia <nancyjg8.cs.washington.edu>
2023-12-07 19:49:30 -0800

    fixing concurrency loading image issue

--- script.js
+++ script.js
@@ -1,26 +1,16 @@
 document.getElementById('imageInput').addEventListener('change', function(event) {
   const file = event.target.files[0];
   const image1 = document.getElementById('image1');
+  const image2 = document.getElementById('image2');
 
-  const formData = new FormData();
-  formData.append('file', file);
-  
-  //saturated
-  fetch('http://127.0.0.1:5000/process_image1', {
-    method: 'POST',
-    body: formData
-  })
-  .then(response => {
-    if (response.ok) {
-      return response.blob();
-    } else {
-      throw new Error('Network response was not ok.');
-    }
-  })
-  .then(blob => {
-    image1.src = URL.createObjectURL(blob);
+  const formData1 = new FormData();
+  formData1.append('file', file);
 
-    fetch('http://127.0.0.1:5000/process_image2', {
+  const formData2 = new FormData();
+  formData2.append('file', file);
+
+  const fetchImage = (url, formData, targetImage) => {
+    return fetch(url, {
       method: 'POST',
       body: formData
     })
@@ -32,15 +22,18 @@ document.getElementById('imageInput').addEventListener('change', function(event)
       }
     })
     .then(blob => {
-      image2.src = URL.createObjectURL(blob);;
+      targetImage.src = URL.createObjectURL(blob);
     })
+    .catch(error => {
+      console.error('There was an error processing the image:', error);
+    });
+  };
 
-  })
+  Promise.all([
+    fetchImage('http://127.0.0.1:5000/process_image1', formData1, image1),
+    fetchImage('http://127.0.0.1:5000/process_image2', formData2, image2)
+  ])
   .catch(error => {
-    console.error('There was an error processing the image:', error);
+    console.error('Error in one of the fetch requests:', error);
   });
-
-  //Black and white
-
-
 });
